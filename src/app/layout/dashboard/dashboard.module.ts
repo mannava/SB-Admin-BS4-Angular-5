2489343b2910fb1ebfa38e0c73cf53b10af0c1a4
bhaskar <bhaskar.mannava@virginamerica.com>
2018-02-28 13:29:11 -0800

    dynamic table updates added.

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective } from 'ng2-table/ng2-table';
+import {
+    Ng2TableModule, NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective,
+    NgTableSortingDirective
+} from 'ng2-table/ng2-table';
 import {
     TimelineComponent,
     NotificationComponent,
     ChatComponent
 } from './components';
 import { StatModule } from '../../shared';
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {TabsModule} from "ng2-bootstrap";
 
 @NgModule({
     imports: [
@@ -18,7 +24,12 @@ import { StatModule } from '../../shared';
         NgbCarouselModule.forRoot(),
         NgbAlertModule.forRoot(),
         DashboardRoutingModule,
-        StatModule
+        StatModule,
+        Ng2TableModule,
+        PaginationModule,
+        FormsModule,
+        PaginationModule,
+        TabsModule
     ],
     declarations: [
         DashboardComponent,
